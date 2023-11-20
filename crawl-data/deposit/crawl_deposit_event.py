8a4aa272235eb74408e2e1c3b8c947ea163e8800
lovetonight <hoan.khoc@gmail.com>
2023-11-20 15:27:07 +0700

    update: return deposit events (aave)

--- crawl-data/deposit/crawl_deposit_event.py
+++ crawl-data/deposit/crawl_deposit_event.py
@@ -14,12 +14,16 @@ def crawl_deposit_event_all(chain="ethereum"):
     # TODO: Tra lai data
 
 
-def crawl_deposit_event(wallet_addresses, chain="ethereum"):
-    
+def crawl_deposit_event(wallet_addresses, chain="ethereum", detail=True):
     """
     input: list address of wallets, name of chain
     output: dict {key: address of wallet, value: total of deposit}
     """
+    # output
+    total_deposit = defaultdict(float)
+    deposit_events = defaultdict(dict)
+
+    # Get event
     connection_url = "mongodb://etlReader:etl_reader_tsKNV6KFr2GWqqqZ@34.126.84.83:27017,34.142.204.61:27017,34.142.219.60:27017"
     connection = MongoClient(connection_url)
     blockchain_etl = connection[f"{chain}_blockchain_etl"]
@@ -34,7 +38,6 @@ def crawl_deposit_event(wallet_addresses, chain="ethereum"):
         amount = tmp.get("amount")
         if token_price_dict.get(token_adress) is None and token_adress is not None:
             token_price_dict[token_adress] = 0.0
-
     # Get price
     connection_url = "mongodb://klgReaderHoan:klg_reader_hoan_123@35.198.222.97:27017,34.124.133.164:27017,34.124.205.24:27017"
     connection = MongoClient(connection_url)
@@ -44,11 +47,24 @@ def crawl_deposit_event(wallet_addresses, chain="ethereum"):
         filter_smartcontract = {"_id": f"0x1_{key}"}
         smart_contracts_object = _smart_contracts.find_one(filter_smartcontract)
         token_price_dict[key] = smart_contracts_object.get("price")
-    total_deposit = defaultdict(float)
+
+
+    # Get deposit tai aave, compound
+    # Processing
     for tmp in deposit_objects:
         wallet_address = tmp.get("wallet")
         token_adress = tmp.get("reserve")
         amount = tmp.get("amount")
         if token_adress is not None:
-            total_deposit[wallet_address] += amount * token_price_dict[token_adress]
-    return total_deposit
+            if detail:
+                if deposit_events[wallet_address].get(token_adress) is not None:
+                    deposit_events[wallet_address][token_adress] += amount
+                else:
+                    deposit_events[wallet_address][token_adress] = amount
+            else:
+                total_deposit[wallet_address] += amount * token_price_dict[token_adress]
+
+    if detail:
+        return deposit_events
+    else:
+        return total_deposit
