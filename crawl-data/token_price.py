5de799ad19c1bb96b9f8fa47a754af43d2528f68
lovetonight <hoan.khoc@gmail.com>
2023-12-04 14:49:02 +0700

    new: export json

--- crawl-data/token_price.py
+++ crawl-data/token_price.py
@@ -14,3 +14,24 @@ def get_token_price_dict(token_addresses):
         smart_contracts_object = _smart_contracts.find_one(filter_smartcontract)
         token_price_dict[address] = smart_contracts_object.get("price")
     return token_price_dict
+
+def export_token_price():
+    # Get price
+    connection_url = "mongodb://klgReaderHoan:klg_reader_hoan_123@35.198.222.97:27017,34.124.133.164:27017,34.124.205.24:27017"
+    connection = MongoClient(connection_url)
+    ethereum_blockchain_lending = connection["knowledge_graph"]
+    _smart_contracts = ethereum_blockchain_lending["smart_contracts"]
+    filter = {"price": {"$gt": 0}, "chainId": "0x1"}
+    price_dict = []
+    price = _smart_contracts.find(filter)
+    for obj in price:
+        price_dict.append(
+            {
+                "address": obj.get("address"),
+                "price": obj.get("price"),
+                "decimals": obj.get("decimals"),
+            }
+        )
+    import json
+    with open(f'../data/price/ethereum.json','w') as f:
+            json.dump(price_dict, f, indent=4)
